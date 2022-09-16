eb579f8e7fd75cc04b793660b19a9646de09455d
EugeniyBurmistrov <psycrab88@gmail.com>
2022-09-16 12:53:11 +0400

    Update solution

---
+++ CS/Solution/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.Web;
+using System;
+
+namespace Solution {
+    public partial class Default : System.Web.UI.Page {
+        protected string GetRowValue(GridViewDataItemTemplateContainer container) {
+            return container.Grid.GetRowValuesByKeyValue(container.KeyValue, "CategoryName").ToString();
+        }
+
+        protected void keyFieldLink_Init(object sender, EventArgs e) {
+            ASPxHyperLink link = sender as ASPxHyperLink;
+            GridViewDataItemTemplateContainer container = link.NamingContainer as GridViewDataItemTemplateContainer;
+            link.Text = "Show New Form, Key Field = " + container.KeyValue;
+            link.Target = "_blank";
+            link.NavigateUrl = "Default2.aspx?id=" + container.KeyValue;
+        }
+    }
+}
---
+++ CS/Solution/Default2.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default2.aspx.cs" Inherits="Solution.Default2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>How to display a hyperlink based on several cell values</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
